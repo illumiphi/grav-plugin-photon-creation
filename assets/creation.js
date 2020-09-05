ddc53f8b81b92e8963ed13d41b444f2cb8f2da5e
i-am-phi <phi@i-am-phi.net>
2020-09-05 11:07:35 -0700

    disable js for now

--- assets/creation.js
+++ assets/creation.js
@@ -3,22 +3,21 @@ var elements = document.querySelectorAll("article.creation figure");
 for (var i = 0; i < elements.length; i++) {
 
 
-  elements[i].addEventListener("mousemove", function(e) {
-    sW = this.offsetWidth;
-    sH = this.offsetHeight;
+  // elements[i].addEventListener("mousemove", function(e) {
+    // sW = this.offsetWidth;
+    // sH = this.offsetHeight;
 
-    x = (200 * (e.offsetX / sW) );
-    // x-= x*.1 + 50
-    y = (200 * (e.offsetY / sH) );
-    // y-= y*.1 + 25
+    // x = (200 * (e.offsetX / sW) );
+    // // x-= x*.1 + 50
+    // y = (200 * (e.offsetY / sH) );
+    // // y-= y*.1 + 25
 
-    cursor = `${x}% ${y}%`;
-    // console.log(cursor);
-    this.querySelector('img').style.objectPosition = cursor;
-  });
+    // cursor = `${x}% ${y}%`;
+    // // console.log(cursor);
+    // this.querySelector('img').style.objectPosition = cursor;
+  // });
 
-  elements[i].addEventListener("mouseleave", function(e) {
-    this.querySelector('img').style.objectPosition = "50% 50%";
-
-  });
+  // elements[i].addEventListener("mouseleave", function(e) {
+    // this.querySelector('img').style.objectPosition = "50% 50%";
+  // });
 }
